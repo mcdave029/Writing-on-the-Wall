67c849dd813a0a71e64df20e45f9cc4bec351f78
Mc Dave Valdecantos <mcdave@gorated.ph>
2020-10-16 20:23:37 +0800

    feat(draggable & bootstrap) install vue draggable and bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container id="app">
+    <draggable v-model="researchableQuestions" group="questions" @start="drag=true" @end="drag=false" class="card-deck">
+    <b-card
+      v-for="question in researchableQuestions"
+      :key="question.id"
+      border-variant="primary"
+      :header="question.name"
+      header-bg-variant="primary"
+      header-text-variant="white"
+      align="center"
+      no-body
+      >
+      <draggable v-model="question.findings" group="findings" @start="drag=true" @end="drag=false" class="card-body">
+      <b-card
+        v-for="finding in question.findings"
+        :key="finding.id"
+        border-variant="info"
+        :header="finding.name"
+        align="center"
+        no-body
+        >
+        <draggable v-model="finding.elements" group="elements" @start="drag=true" @end="drag=false" class="card-body">
+        <b-list-group-item v-for="element in finding.elements" :key="element.id">
+          <b-form-input v-model="element.name" placeholder="Enter your name"></b-form-input>
+        </b-list-group-item>
+        </draggable>
+      </b-card>
+      </draggable>
+    </b-card>
+    </draggable>
+    {{researchableQuestions}}
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from "vuedraggable";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    draggable
+  },
+  data() {
+    return {
+      researchableQuestions: [
+        {
+          id: 1,
+          name: "Question 1",
+          findings: [
+            {
+              name: "Find 1 Q1",
+              elements: [
+                {
+                  name: "Element F1 Q1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "Question 2",
+          findings: [
+            {
+              id: 1,
+              name: "Find 1 Q2",
+              elements: [
+                {
+                  id: 1,
+                  name: "Element F1 Q2"
+                }
+              ]
+            },{
+              id: 2,
+              name: "Find 2 Q2",
+              elements: [
+                {
+                  id: 1,
+                  name: "Element F2 Q2"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/nested.vue
@@ -0,0 +1,24 @@
+<template>
+  <draggable :list="tasks" :group="{ name: 'g1' }" class="list-group">
+      <b-list-group-item v-for="el in tasks" :key="el.name">
+        <p>{{el.name}}</p>
+        <nested-draggable :tasks="el.tasks" />
+      </b-list-group-item>
+   </draggable>
+</template>
+<script>
+import draggable from "vuedraggable";
+export default {
+  props: {
+    tasks: {
+      required: true,
+      type: Array
+    }
+  },
+  components: {
+    draggable
+  },
+  name: "nested-draggable"
+};
+</script>
+<style scoped></style>
